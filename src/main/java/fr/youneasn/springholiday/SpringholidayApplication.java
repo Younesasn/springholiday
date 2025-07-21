ff101268ba6cdb1bbf7f51ab0b961cfdfba22ee1
ASSANI Younes <younesassani@gmail.com>
2025-07-21 10:12:39 +0200

    ajout des dépendances

---
+++ src/main/java/fr/youneasn/springholiday/SpringholidayApplication.java
@@ -0,0 +1,13 @@
+package fr.youneasn.springholiday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringholidayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringholidayApplication.class, args);
+	}
+
+}
